3194780e2bae0041658bddbd74363caeb3d47230
Bireower <65842769+Bireower@users.noreply.github.com>
2021-05-20 21:16:18 +0800

    Add files via upload

---
+++ js/CreateForm.js
@@ -0,0 +1,68 @@
+function CreateForm(){
+    //this.col = 3;
+}
+
+function CreateWordList(word,trans)
+{
+    this.dom = document.createElement("table");
+    var tr,td;
+    tr = document.createElement("tr");
+    tr.className = "lineOfwords"
+    for (var i =0;i<3;i++)
+    {
+        td = document.createElement("td");
+        if (i ==0)
+        {
+            td.className ="wordName-td"
+            td.innerHTML = word
+        }
+        if (i ==1)
+        {
+            td.className ="wordTrans-td"
+            td.innerHTML = trans
+        }
+        if (i==2)
+        {
+            td.className = "wordDel-td"
+            btn = document.createElement("button")
+            span = document.createElement("span")
+            btn.className = "delWordBtn"
+            span.className ="iconfont" 
+            span.innerHTML ="&#xe61f;"
+            btn.appendChild(span)
+            td.appendChild(btn)
+        }
+
+        tr.appendChild(td)
+    }
+    this.dom.appendChild(tr);
+    document.getElementById("wordList").appendChild(this.dom);
+}
+
+function AddLineWord() 
+{
+    var inputWord = document.getElementById("wordLineEdit")
+
+    this.dom = document.createElement("table");
+    var tr,td;
+    tr = document.createElement("tr");
+    tr.className = "lineOfwords"
+    for (var i =0;i<2;i++)
+    {
+        td = document.createElement("td");
+        if (i ==0)
+        {
+            td.className ="wordName"
+            td.innerHTML = inputWord.value
+        }
+        if (i ==1)
+        {
+            td.className ="wordParaphrase"
+            td.innerHTML = "paraphrase"
+        }
+        tr.appendChild(td)
+    }
+    this.dom.appendChild(tr);
+    document.getElementById("wordList").appendChild(this.dom);
+    inputWord.value=""
+}
