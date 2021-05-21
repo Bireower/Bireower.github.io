b8c77b9ed28bda0c8a27a6578d35a500ef6a8e68
Bireower <501005719@qq.com>
2021-05-21 09:57:18 +0800

    tmpSummit

--- js/LoadLocalData.js
+++ js/LoadLocalData.js
@@ -1,24 +1,29 @@
-function UpdataFile(){
-    var input = document.getElementById("uploadFile");
-    var reader = new FileReader();
-    var jsonFile
+// function UpdataFile(){
+//     var input = document.getElementById("uploadFile");
+//     var reader = new FileReader();
+//     var jsonFile
 
-    reader.readAsText(input.files[0]);
-    reader.onload = function () 
-    {
-        jsonFile=JSON.parse(reader.result) 
+//     reader.readAsText(input.files[0]);
+//     reader.onload = function () 
+//     {
+//         jsonFile=JSON.parse(reader.result) 
 
-        for(var i =0;i<jsonFile.length;i++)
-        {
-            CreateWordList(jsonFile[i].word,jsonFile[i].trans)
-        }
-    };
-}
+//         for(var i =0;i<jsonFile.length;i++)
+//         {
+//             CreateWordList(jsonFile[i].word,jsonFile[i].trans)
+//         }
+//     };
+// }
 
 
 
 function LoadLocalFile()
 {
+    if (document.getElementById("wordList").contains("lineOfwords"))
+    {
+        alert(true)
+        return;
+    }
     var jsonFile
 
     $.getJSON("data/2021-05-21.json",function(data){
